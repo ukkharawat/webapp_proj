2168ed3e4f0d371a1b96fa2b3e9f0abf5c59a2cd
thetongz <miiz_tongz@live.com>
2017-04-16 13:17:58 +0700

    delete some function use function from other file

--- controller/cosmetic_api.js
+++ controller/cosmetic_api.js
@@ -1,13 +1,13 @@
 var cosmetics = require('../database/cosmetic')
 var mongoose = require('mongoose')
 var dbconfig = require('../config/database')
+var fc = require('../config/function')
 
 module.exports.getCosmetics = function(req,res){
 	if(!req.query.search){
 		return
 	}
-	var search = (req.query.search).toLowerCase()
-	search = search.charAt(0).toUpperCase() + search.slice(1)
+	var search = fc.stringForm(String(req.query.search))
 	mongoose.connect(dbconfig.url)
 	cosmetics.find({ $or : [ {brand: search}, {name: search}, {type: search}, {collections: search} ]}, function(err, data){
 		if(data.length != 0){
@@ -19,19 +19,15 @@ module.exports.getCosmetics = function(req,res){
 	})
 }
 
-function stringForm(str){
-	return str.charAt(0).toUpperCase() + str.toLowerCase().slice(1)
-}
-
 module.exports.addCosmetics = function(req,res){
 	if(!req.body.brand || !req.body.type || !req.body.collections || !req.body.name || !req.body.detail){
 		res.json({message: "Every field can't be empty"})
 	}
 	if(req.cookies.auth == 1){
-		var brand = stringForm(String(req.body.brand))
-		var type = stringForm(String(req.body.type))
-		var collections = stringForm(String(req.body.collections))
-		var name = stringForm(String(req.body.name))
+		var brand = fc.stringForm(String(req.body.brand))
+		var type = fc.stringForm(String(req.body.type))
+		var collections = fc.stringForm(String(req.body.collections))
+		var name = fc.stringForm(String(req.body.name))
 		mongoose.connect(dbconfig.url)
 		var cosmetic = new cosmetics({
 			brand: brand,
