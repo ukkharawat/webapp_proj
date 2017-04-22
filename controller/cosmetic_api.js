ebb58b16c5d946ecd166ed8e8e18f5094e40a4c6
ukkharawat uimpanchai <ukkharawat uimpanchai>
2017-04-22 13:44:06 +0700

    add editCosmetics

--- controller/cosmetic_api.js
+++ controller/cosmetic_api.js
@@ -3,20 +3,46 @@ var mongoose = require('mongoose')
 var dbconfig = require('../config/database')
 var fc = require('../config/function')
 
+/*
+	getCosmetic
+	addCosmetic
+	editCosmetic
+*/
+
 module.exports.getCosmetics = function(req,res){
-	if(!req.query.search){
-		return
-	}
-	var search = fc.stringForm(String(req.query.search))
 	mongoose.connect(dbconfig.url)
-	cosmetics.find({ $or : [ {brand: search}, {name: search}, {type: search}, {collections: search} ]}, function(err, data){
-		if(data.length != 0){
-			res.json(data)
-		}else{
-			res.json({message: "not found"})
-		}
+	cosmetics.find({}, function(err, data){
+		res.json(data)
 		mongoose.disconnect()
 	})
+}
+
+module.exports.editCosmetics = function(req,res){
+	if(!req.body.brand || !req.body.type || !req.body.collections || !req.body.name || !req.body.detail){
+		res.json({message: "Every field can't be empty"})
+	}
+	console.log(req.body)
+	if(req.cookies.auth == 1){
+		var brand = fc.stringForm(String(req.body.brand))
+		var type = fc.stringForm(String(req.body.type))
+		var collections = fc.stringForm(String(req.body.collections))
+		var name = fc.stringForm(String(req.body.name))
+		mongoose.connect(dbconfig.url)
+		cosmetics.find({id:req.body.id} , function(err,data){
+			console.log(data)
+			if(!err){
+				data[0].brand = brand
+				data[0].type = type
+				data[0].collections = collections
+				data[0].name = name
+				data[0].detail = String(req.body.detail)
+				data[0].save(function(err,data){
+					res.json(data)
+					mongoose.disconnect()
+				})
+			}
+		})
+	}
 }
 
 module.exports.addCosmetics = function(req,res){
