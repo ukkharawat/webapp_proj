2774f4c79b678714fd3abc78c17dfd2766676333
thetongz <miiz_tongz@live.com>
2017-05-11 09:46:21 +0700

    add some attribute to user

--- router/user.js
+++ router/user.js
@@ -70,6 +70,7 @@ routes.post('/login', function(req, res) {
                         user: {
                             id: user._id,
                             username: user.username,
+                            authen: user.authen
                         }
                     })
                 } else {
