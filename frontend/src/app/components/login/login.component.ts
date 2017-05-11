2703818fbd28492dfd066644e927508279948c45
thetongz <miiz_tongz@live.com>
2017-05-11 09:46:32 +0700

    front : implement login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service'
+import { FlashMessagesService } from 'angular2-flash-messages'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -7,12 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private flashMessage:FlashMessagesService,
+    private authService:AuthService,
+    private router:Router
+  ) { }
+
+  username : String
+  password : String
+  
+
 
   ngOnInit() {
   }
 
-  public checkSubmit(event:any) :void {
-    console.log("hello")
+  
+
+
+  public login(event:any) :void {
+    const user = {
+      username : this.username,
+      password : this.password
+    }
+
+    this.authService.loginUser(user).subscribe(data => {
+      if(data.success){
+        this.authService.storeUserData(data.token, data.user)
+        this.flashMessage.show('You are now logged in', {
+          cssClass: 'alert-success',
+          timeout: 5000})
+        this.router.navigate(['dashboard'])
+      }else{
+        this.flashMessage.show(data.message, {
+          cssClass: 'alert-danger',
+          timeout: 5000})
+        this.router.navigate(['login'])
+      }
+    })
+
+
   }
 }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -22,4 +22,17 @@ export class AuthService {
       .map(res => res.json());
   }
 
+  loginUser(user){
+    let headers = new Headers()
+    headers.append('Content-Type' , 'application/json')
+    return this.http.post('http://localhost:3000/user/login' , user , {headers : headers})
+        .map(res => res.json())
+  }
+
+   storeUserData(token, user){
+    localStorage.setItem('id_token', token)
+    localStorage.setItem('user', JSON.stringify(user))
+    this.authToken = token
+    this.user = user
+  }
 }
