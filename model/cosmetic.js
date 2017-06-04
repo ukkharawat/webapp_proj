36e4a464d4ccc8259df020328f6713c3a77ccdc4
thetongz <miiz_tongz@live.com>
2017-06-04 14:12:50 +0700

    change some query

--- model/cosmetic.js
+++ model/cosmetic.js
@@ -6,42 +6,42 @@ var cosmetic = mongoose.Schema({
     brand: String,
     category: String,
     quality: String,
-    color: [{type: String}],
-    name: {type: String , unique: true , index:true},
+    color: [{ type: String }],
+    name: { type: String, unique: true, index: true },
     detail: String,
     like: {
         count: Number,
-        who: [{type: String , ref: 'User'}] // collect who like it
+        who: [{ type: String, ref: 'User' }] // collect who like it
     }
 })
 
-cosmetic.plugin(autoIncrement, {inc_field: 'id'})
+cosmetic.plugin(autoIncrement, { inc_field: 'id' })
 
 var Cosmetic = module.exports = mongoose.model('Cosmetic', cosmetic)
 
-module.exports.getAllCosmetic = function(callback){
-    Cosmetic.find({},callback)
+module.exports.getAllCosmetic = function(callback) {
+    Cosmetic.find({}, callback)
 }
 
-module.exports.getSortCosmetic = function(callback){
-    Cosmetic.find({}).sort(({id : -1})).exec(callback)
+module.exports.getSortCosmetic = function(callback) {
+    Cosmetic.find({}).sort(({ id: -1 })).exec(callback)
 }
 
-module.exports.getCosmeticByCategory = function(category ,callback){
-    var query = {category : category}
-    Cosmetic.find(query , callback)
+module.exports.getCosmeticByCategory = function(category, callback) {
+    var query = { category: category }
+    Cosmetic.find(query).select({ name: 1, brand: 1, image: 1, like: 1 }).exec(callback)
 }
 
-module.exports.getCosmeticByBrand = function(brand ,callback){
-    var query = {brand : brand}
-    Cosmetic.find(query , callback)
+module.exports.getCosmeticByBrand = function(brand, callback) {
+    var query = { brand: brand }
+    Cosmetic.find(query, callback)
 }
 
-module.exports.addCosmetic = function(cosmetic , callback){
+module.exports.addCosmetic = function(cosmetic, callback) {
     cosmetic.save(callback)
 }
 
-module.exports.getById = function(id , callback){
-    var query = {id : id}
-    Cosmetic.findOne(query , callback)
+module.exports.getById = function(id, callback) {
+    var query = { id: id }
+    Cosmetic.findOne(query, callback)
 }
--- router/index.js
+++ router/index.js
@@ -12,102 +12,102 @@ var passport = require('passport')
 	editCosmetic
 	addToWishlist
 */
-function checkAdminAuthen(req,res,next){
-    if(req.user.authen == 1){
+function checkAdminAuthen(req, res, next) {
+    if (req.user.authen == 1) {
         next()
     }
 }
 
-routes.get('/',function(req,res){
-    res.sendFile(path.join(__dirname , '../public/index.html'))
+routes.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
-routes.get('/getCosmetics', function(req,res){
-    cosmetics.getAllCosmetic(function(err,data){
-		res.json(data)
-	})
+routes.get('/getCosmetics', function(req, res) {
+    cosmetics.getAllCosmetic(function(err, data) {
+        res.json(data)
+    })
 })
 
-routes.get('/getNewCosmetics', function(req,res){
-	cosmetics.getSortCosmetic(function(err , data){
-		res.json(data)
-	})
+routes.get('/getNewCosmetics', function(req, res) {
+    cosmetics.getSortCosmetic(function(err, data) {
+        res.json(data)
+    })
 })
 
-routes.get('/getCosmeticsByCategory' , function(req,res){
-	cosmetics.getCosmeticByCategory(req.query.category , function(err ,data){
-		res.json(data)
-	})
+routes.get('/getCosmeticsByCategory/:category', function(req, res) {
+    cosmetics.getCosmeticByCategory(req.params.category, function(err, data) {
+        res.json(data)
+    })
 })
 
-routes.get('/getCosmeticsByBrand' , function(req,res){
-	cosmetics.getCosmeticByBrand(req.query.brand , function(err ,data){
-		res.json(data)
-	})
+routes.get('/getCosmeticsByBrand', function(req, res) {
+    cosmetics.getCosmeticByBrand(req.query.brand, function(err, data) {
+        res.json(data)
+    })
 })
 
-routes.get('/addToWishlist' , passport.authenticate('jwt', {session:false}) , function(req,res){
-	users.getUserById(req.user._id , function(err , data){
-		var isContain = false
-		for(var i = 0 ; i < data.wishlist.length ; i++){
-			if(data.wishlist[i].id == req.query.id){
-				isContain = true
-				var index = i
-				break
-			}
-		}
-		if(isContain){
-			data.wishlist.splice( index, 1 )
-		}else{
-			data.wishlist.push({
-				name : req.query.name,
-				id : req.query.id
-			})
-		}
-		data.save(function(err , data){
-			res.json(data)
-		})
-	})
+routes.get('/addToWishlist', passport.authenticate('jwt', { session: false }), function(req, res) {
+    users.getUserById(req.user._id, function(err, data) {
+        var isContain = false
+        for (var i = 0; i < data.wishlist.length; i++) {
+            if (data.wishlist[i].id == req.query.id) {
+                isContain = true
+                var index = i
+                break
+            }
+        }
+        if (isContain) {
+            data.wishlist.splice(index, 1)
+        } else {
+            data.wishlist.push({
+                name: req.query.name,
+                id: req.query.id
+            })
+        }
+        data.save(function(err, data) {
+            res.json(data)
+        })
+    })
 })
 
-routes.post('/addCosmetic'  , passport.authenticate('jwt', {session:false}) , checkAdminAuthen , function(req,res){
-	var file = req.files.file
-	var image = file.name
-	file.mv(path.join(__dirname , '../public/cosmetic_image/' , fc.stringForm(req.body.name) + "_image." + image.split('.').pop()))
-	var cosmetic = new cosmetics({
-		image: fc.stringForm(req.body.name) + "_image." + image.split('.').pop(),
-		brand: fc.stringForm(req.body.brand),
-		category: fc.stringForm(req.body.category),
-		quality: req.body.quality,
-		color: req.body.color,
-		name: fc.stringForm(req.body.name),
-		detail: req.body.detail,
-		like : {
-			count: 0,
-			who: []
-		}
-	})
-	cosmetics.addCosmetic(cosmetic , function(err , data){
-		res.json(data)
-	})
+routes.post('/addCosmetic', passport.authenticate('jwt', { session: false }), checkAdminAuthen, function(req, res) {
+    var file = req.files.file
+    var image = file.name
+    file.mv(path.join(__dirname, '../public/cosmetic_image/', fc.stringForm(req.body.name) + "_image." + image.split('.').pop()))
+    var cosmetic = new cosmetics({
+        image: fc.stringForm(req.body.name) + "_image." + image.split('.').pop(),
+        brand: fc.stringForm(req.body.brand),
+        category: fc.stringForm(req.body.category),
+        quality: req.body.quality,
+        color: req.body.color,
+        name: fc.stringForm(req.body.name),
+        detail: req.body.detail,
+        like: {
+            count: 0,
+            who: []
+        }
+    })
+    cosmetics.addCosmetic(cosmetic, function(err, data) {
+        res.json(data)
+    })
 })
 
-routes.post('/editCosmetic'  , passport.authenticate('jwt', {session:false}) , checkAdminAuthen , function(req,res){
-	cosmetics.getById(req.body.id , function(err , data){
-		var file = req.files.file
-		var image = file.name
-		file.mv(path.join(__dirname , '../public/cosmetic_image/' , fc.stringForm(req.body.name) + "_image." + image.split('.').pop()))
-		data.image = fc.stringForm(req.body.name) + "_image." + image.split('.').pop()
-		data.brand = fc.stringForm(req.body.brand)
-		data.category = fc.stringForm(req.body.category)
-		data.quality = fc.stringForm(req.body.quality)
-		data.color = req.body.color
-		data.name = req.body.name
-		data.detail = req.body.detail
-		cosmetics.addCosmetic(data , function(err ,data){
-			res.json(data)
-		})
-	})
+routes.post('/editCosmetic', passport.authenticate('jwt', { session: false }), checkAdminAuthen, function(req, res) {
+    cosmetics.getById(req.body.id, function(err, data) {
+        var file = req.files.file
+        var image = file.name
+        file.mv(path.join(__dirname, '../public/cosmetic_image/', fc.stringForm(req.body.name) + "_image." + image.split('.').pop()))
+        data.image = fc.stringForm(req.body.name) + "_image." + image.split('.').pop()
+        data.brand = fc.stringForm(req.body.brand)
+        data.category = fc.stringForm(req.body.category)
+        data.quality = fc.stringForm(req.body.quality)
+        data.color = req.body.color
+        data.name = req.body.name
+        data.detail = req.body.detail
+        cosmetics.addCosmetic(data, function(err, data) {
+            res.json(data)
+        })
+    })
 })
 
 module.exports = routes
