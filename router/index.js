eea660463f036164e23648b1df814750e83954e9
thetongz <miiz_tongz@live.com>
2017-06-05 15:05:26 +0700

    add multer to upload cos img

--- router/index.js
+++ router/index.js
@@ -3,6 +3,15 @@ var cosmetics = require('../model/cosmetic')
 var users = require('../model/user')
 var path = require('path')
 var passport = require('passport')
+var multer = require('multer')
+var DIR = 'public/cosmetic_image/'
+var fs = require('fs')
+
+function checkAdminAuthen(req, res, next) {
+    if (req.user.authen == 1) {
+        next()
+    }
+}
 
 /*
 	getCosmetic
@@ -12,11 +21,23 @@ var passport = require('passport')
 	editCosmetic
 	addToWishlist
 */
-function checkAdminAuthen(req, res, next) {
-    if (req.user.authen == 1) {
-        next()
+
+
+var storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, `${DIR}/`)
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.originalname)
     }
-}
+})
+
+var upload = multer({ storage: storage })
+
+routes.post('/upload', upload.any(), function(req, res) {
+    res.json({ message: "upload success" })
+})
+
 
 routes.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'))
@@ -71,12 +92,13 @@ routes.get('/addToWishlist', passport.authenticate('jwt', { session: false }), f
 })
 
 routes.post('/addCosmetic', passport.authenticate('jwt', { session: false }), checkAdminAuthen, function(req, res) {
-    var file = req.files.file
-    var image = file.name
-    file.mv(path.join(__dirname, '../public/cosmetic_image/', fc.stringForm(req.body.name) + "_image." + image.split('.').pop()))
+    var displayImage = req.body.displayImage
+    fs.renameSync(path.join(__dirname, '../public/cosmetic_image/' + displayImage),
+        path.join(__dirname, '../public/cosmetic_image/' + req.body.name + "_image." + displayImage.split('.').pop()))
+
     var cosmetic = new cosmetics({
-        image: fc.stringForm(req.body.name) + "_image." + image.split('.').pop(),
-        brand: fc.stringForm(req.body.brand),
+        image: fc.stringForm(req.body.name) + "_image." + displayImage.split('.').pop(),
+        brand: req.body.brand,
         category: fc.stringForm(req.body.category),
         quality: req.body.quality,
         color: req.body.color,
@@ -88,7 +110,11 @@ routes.post('/addCosmetic', passport.authenticate('jwt', { session: false }), ch
         }
     })
     cosmetics.addCosmetic(cosmetic, function(err, data) {
-        res.json(data)
+        if (!err) {
+            res.json({ message: "success" })
+        } else {
+            res.json({ message: "false" })
+        }
     })
 })
 
