b4ecaf26a66eb071427b9c7d5cd650e198c3d1f7
thetongz <miiz_tongz@live.com>
2017-06-07 22:15:54 +0700

    add getById and save for edit

--- model/cosmetic.js
+++ model/cosmetic.js
@@ -41,3 +41,16 @@ module.exports.addCosmetic = function(cosmetic, callback) {
 module.exports.getByIds = function(id, callback) {
     Cosmetic.find().where({ id: { $in: id } }).select({ id: 1, name: 1, brand: 1, image: 1 }).exec(callback)
 }
+
+module.exports.getCosmeticById = function(id, callback) {
+    var query = { id: id }
+    Cosmetic.findOne(query, callback)
+}
+
+module.exports.save = function(cosmetic, callback) {
+    var query = { id: cosmetic.id }
+    Cosmetic.findOne(query, function(err, data) {
+        data = cosmetic
+        data.save(callback)
+    })
+}
