f1b51e8622de8a582453f4d77fdd0c6c6b9b204c
thetongz <miiz_tongz@live.com>
2017-06-08 15:46:39 +0700

    complete review , View and Like don't

--- frontend/src/app/components/detail/detail.component.ts
+++ frontend/src/app/components/detail/detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CosmeticService } from '../../services/cosmetic.service'
+import { ReviewService } from '../../services/review.service'
 import { AuthService } from '../../services/auth.service'
 import { FlashMessagesService } from 'angular2-flash-messages'
 
@@ -13,25 +14,56 @@ export class DetailComponent implements OnInit {
   
   cosmetic = []
   reviews = []
+  username : String
+  rating : Number = 0
+  comment : String
+  id : Number
 
   constructor(
     private flashMessage: FlashMessagesService,
     private router: Router,
     private route: ActivatedRoute,
     private cosmeticService: CosmeticService,
+    private reviewService : ReviewService,
     private authService: AuthService
   ) { }
 
   ngOnInit() {
+    
     this.route.params.subscribe( params => {
-        this.cosmeticService.getCosmeticById(params['id']).subscribe( data => {
+      this.id = params['id']  
+        this.cosmeticService.getCosmeticById(this.id).subscribe( data => {
           this.cosmetic.push(data)
+          var user = this.authService.getUser()
+          if(user){
+            this.username = user.username
+          }
         })
     })
   }
 
   goToCosmetic(){
     this.router.navigate(['cosmetic' , this.cosmetic[0].category])
+  }
+
+  onSelectionChange($event){
+    this.rating = Number($event.target.id)
+  }
+
+  review(){
+    var review = {
+      starPoint : this.rating,
+      content : this.comment
+    }
+    this.reviewService.review(review).subscribe( data => {
+        if(data.message){
+          this.flashMessage.show('Review done', { cssClass: 'alert-success', timeout: 3000 })
+          window.scrollTo(0,0)
+          location.reload()
+        }else{
+          this.flashMessage.show('Something went wrong', { cssClass: 'alert-danger', timeout: 3000 })
+        }
+    })
   }
 
 }
--- router/review.js
+++ router/review.js
@@ -10,86 +10,82 @@ var passport = require('passport')
 */
 
 
-routes.post('/review' , passport.authenticate('jwt', {session:false}) ,  function(req,res){
+routes.post('/review', passport.authenticate('jwt', { session: false }), function(req, res) {
     var newreview = {
-        reviewer : req.user.username,
-        content : req.body.content,
-        date : new Date(),
-        starPoint : req.body.starPoint,
-        like : {
-            count : 0,
-            who : []
+        reviewer: req.user.username,
+        content: req.body.content,
+        date: new Date(),
+        starPoint: req.body.starPoint,
+        like: {
+            count: 0,
+            who: []
         }
     }
-    reviews.getAllReview(req.body.cosmetic_name , function(err ,review){
-        if(!review){
+    reviews.getAllReview(req.body.cosmetic_name, function(err, review) {
+        if (!review) {
             review = new reviews({
-                cosmetic_name : req.body.cosmetic_name,
-                review : newreview
+                cosmetic_name: req.body.cosmetic_name,
+                review: newreview
             })
-        }else{
+        } else {
             review.review.push(newreview)
         }
-        reviews.review(review , function(err , data){
-            res.json(data)
+        reviews.review(review, function(err, data) {
+            res.json({ message: err ? false : true })
         })
     })
 })
 
-routes.get('/getAllReview',  function(req,res){
-    reviews.getAllReview(req.query.cosmetic_name , function(err , review){
-        if(!review){
-            res.json({message : "No comment in this cosmetic"})
-        }else{
-            var sort_review = []
-            for(var i = 0 ; i < review.review.length ; i++){
-                sort_review.push(review.review[i])
+routes.get('/getAllReview', function(req, res) {
+    reviews.getAllReview(req.query.cosmetic_name, function(err, review) {
+        if (!review) {
+            res.json({ message: "No comment in this cosmetic" })
+        } else {
+            var review = []
+            for (var i = 0; i < review.review.length; i++) {
+                review.push(review.review[i])
             }
-            sort_review.sort((a,b) => {
-                return -(a.like.count - b.like.count)
-            })
-            res.json(sort_review)
+            res.json(review.sort((a, b) => b.like.count - a.like.count))
         }
     })
 })
 
-routes.get('/likeReview' , passport.authenticate('jwt', {session:false}) , function(req,res){
-    console.log("Hello")
-    reviews.getReviewById(req.query.id , function(err , data){
-        for(var i = 0 ; i < data.review.length ; i++){
-            if(data.review[i]._id == req.query.idReview){
-                var index = i 
+routes.get('/likeReview', passport.authenticate('jwt', { session: false }), function(req, res) {
+    reviews.getReviewById(req.query.id, function(err, data) {
+        for (var i = 0; i < data.review.length; i++) {
+            if (data.review[i]._id == req.query.idReview) {
+                var index = i
                 var isContain = false
-                for(var j = 0 ; j < data.review[i].like.who.length ; j++){
-                    if(data.review[i].like.who[j] == req.user.username){
+                for (var j = 0; j < data.review[i].like.who.length; j++) {
+                    if (data.review[i].like.who[j] == req.user.username) {
                         isContain = true
                         data.review[index].like.count--
-                        data.review[index].like.who.splice(j, 1 )
+                            data.review[index].like.who.splice(j, 1)
                         break
                     }
                 }
                 break
             }
         }
-        if(!isContain){
+        if (!isContain) {
             data.review[index].like.count++
-            data.review[index].like.who.push(req.user.username)
+                data.review[index].like.who.push(req.user.username)
         }
-        reviews.review(data , function(err , data){
-            res.json(data)
+        reviews.review(data, function(err, data) {
+            res.json({ message: err ? false : true })
         })
     })
 })
 
-routes.post('/editReview' , passport.authenticate('jwt', {session:false}) ,  function(req,res){
-    reviews.getReviewById(req.body.id , function(err , review){
-        for(var i = 0 ; i < review.review.length ; i++){
-            if(data.review[i]._id == req.query.idReview && review.review[i].reviewer == req.user.username){
+routes.post('/editReview', passport.authenticate('jwt', { session: false }), function(req, res) {
+    reviews.getReviewById(req.body.id, function(err, review) {
+        for (var i = 0; i < review.review.length; i++) {
+            if (data.review[i]._id == req.query.idReview && review.review[i].reviewer == req.user.username) {
                 review.review[i].content = req.body.newcontent
                 review.review[i].starPoint = req.body.starPoint
                 review.review[i].date = new Date()
-                reviews.review(review , function(err , data){
-                    res.json(data)
+                reviews.review(review, function(err, data) {
+                    res.json({ message: err ? false : true })
                 })
                 break
             }
