72936fbec5b05675021cfe4e4488df5414abcd51
thetongz <miiz_tongz@live.com>
2017-06-08 13:10:06 +0700

    show detail now work , Rating and Comment under consturction

--- frontend/src/app/components/detail/detail.component.ts
+++ frontend/src/app/components/detail/detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CosmeticService } from '../../services/cosmetic.service'
+import { AuthService } from '../../services/auth.service'
+import { FlashMessagesService } from 'angular2-flash-messages'
 
 @Component({
   selector: 'app-detail',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
+  
+  cosmetic = []
+  reviews = []
 
-  constructor() { }
+  constructor(
+    private flashMessage: FlashMessagesService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private cosmeticService: CosmeticService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe( params => {
+        this.cosmeticService.getCosmeticById(params['id']).subscribe( data => {
+          this.cosmetic.push(data)
+        })
+    })
+  }
+
+  goToCosmetic(){
+    this.router.navigate(['cosmetic' , this.cosmetic[0].category])
   }
 
 }
